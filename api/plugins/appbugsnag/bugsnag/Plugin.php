5662cecefc72009570864cf19a52da5a06103479
Mato <martinmojzis06@gmail.com>
2024-12-14 22:07:21 +0100

    add bugsnag statistics

---
+++ api/plugins/appbugsnag/bugsnag/Plugin.php
@@ -0,0 +1,50 @@
+<?php namespace AppBugsnag\Bugsnag;
+
+use Bugsnag\Client;
+use Bugsnag\Handler;
+use System\Classes\PluginBase;
+
+/**
+ * Bugsnag Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Bugsnag',
+            'description' => 'No description provided yet...',
+            'author'      => 'AppBugsnag',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+		//
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+        $bugsnag = Client::make(env('BUGSNAG_API_KEY'));
+        $bugsnag->setReleaseStage(env('APP_ENV'));
+        $bugsnag->setAppVersion(env('APP_VERSION'));
+        $bugsnag->startSession();
+        Handler::register($bugsnag);
+    }
+}
