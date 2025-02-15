438f064da578b8fbff619ca88f3a981f7fa66ada
Lasicak1 <martinsimontkac@gmail.com>
2025-02-15 23:07:10 +0100

    ad wip

---
+++ app/src/components/app@_components/car-info.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex items-center flex-row">
+        <div v-html="icon"></div>
+        <div class="ml-2 gap-12 flex">
+            <h6 class="w-[107px]" >{{ carInfo }}</h6>
+            <p >{{ carInfoData }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props: {
+        icon: {
+            type: String,
+            required: true,
+        },
+        carInfo: {
+            type: String,
+            required: true,
+        },
+        carInfoData: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>
