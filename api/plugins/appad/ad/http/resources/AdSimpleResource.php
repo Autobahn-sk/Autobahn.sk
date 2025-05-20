d9df0b590cfddede70abef3a64e856df25a94d6f
Martin Mojzis <martinmojzis06@gmail.com>
2025-05-20 23:14:15 +0200

    response

--- api/plugins/appad/ad/http/resources/AdSimpleResource.php
+++ api/plugins/appad/ad/http/resources/AdSimpleResource.php
@@ -1,6 +1,7 @@
 <?php namespace AppAd\Ad\Http\Resources;
 
 use October\Rain\Support\Facades\Event;
+use AppUtil\Util\Http\Resources\FileResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AdSimpleResource extends JsonResource
@@ -12,8 +13,8 @@ class AdSimpleResource extends JsonResource
 			'title' 			=> $this->title,
 			'slug' 				=> $this->slug,
 			'summary' 			=> $this->summary,
-			'thumbnail' 		=> $this->thumbnail,
 			'badge' 			=> $this->badge,
+			'thumbnail' 		=> new FileResource($this->thumbnail),
 			'current_price'     => $this->current_price?->price,
 			'mileage' 			=> $this->vehicle?->mileage,
 			'fuel_type' 		=> $this->vehicle?->fuel_type,
